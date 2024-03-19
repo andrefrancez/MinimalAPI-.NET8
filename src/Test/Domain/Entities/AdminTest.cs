601b869d8e350e8adcc9b2c9b8ae39126baf2810
Andre Francez <andregfrancez@gmail.com>
2024-03-19 11:58:38 -0300

    Entity classes test

---
+++ src/Test/Domain/Entities/AdminTest.cs
@@ -0,0 +1,27 @@
+﻿using project.Domain.Entities;
+
+namespace Test.Domain.Entities
+{
+    [TestClass]
+    public class AdminTest
+    {
+        [TestMethod]
+        public void TestGetSetAdminProperties()
+        {
+            // Arrange
+            var adm = new Admin();
+
+            // Act
+            adm.Id = 1;
+            adm.Email = "test@test.com";
+            adm.Password = "test123";
+            adm.Profile = "Editor";
+
+            // Assert
+            Assert.AreEqual(1, adm.Id);
+            Assert.AreEqual("test@test.com", adm.Email);
+            Assert.AreEqual("test123", adm.Password);
+            Assert.AreEqual("Editor", adm.Profile);
+        }
+    }
+}
---
+++ src/Test/Domain/Entities/VehicleTest.cs
@@ -0,0 +1,33 @@
+﻿using project.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Domain.Entities
+{
+    [TestClass]
+    public class VehicleTest
+    {
+        [TestMethod]
+        public void TestGetSetVehicleProperties()
+        {
+            var vehicle = new Vehicle();
+
+            vehicle.Id = 5;
+            vehicle.Name = "Civic";
+            vehicle.Make = "Honda";
+            vehicle.ModelYear = 2014;
+            vehicle.Color = "Red";
+            //vehicle.Description = "Description";
+
+            Assert.AreEqual(5, vehicle.Id);
+            Assert.AreEqual("Civic", vehicle.Name);
+            Assert.AreEqual("Honda", vehicle.Make);
+            Assert.AreEqual(2014, vehicle.ModelYear);
+            Assert.AreEqual("Red", vehicle.Color);
+            //Assert.AreEqual("Description", vehicle.Description);
+        }
+    }
+}
