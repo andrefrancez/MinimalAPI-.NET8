1d1c69468be12c86aa4b7f8b77b63ff04ef70b32
Andre Francez <andregfrancez@gmail.com>
2024-03-22 06:22:02 -0300

    Refactored testing project

---
+++ src/TestProject/Domain/Entities/AdminTest.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using project.Domain.Entities;
+
+namespace TestProject.Domain.Entities
+{
+    public class AdminTest
+    {
+        [Fact]
+        public void TestGetSetAdminProperties()
+        {
+            // Arrange
+            var mockAdmin = new Mock<Admin>();
+
+            // Act
+            mockAdmin.Object.Id = 1;
+            mockAdmin.Object.Email = "test@test.com";
+            mockAdmin.Object.Password = "test123";
+            mockAdmin.Object.Profile = "Editor";
+
+            // Assert
+            Assert.Equal(1, mockAdmin.Object.Id);
+            Assert.Equal("test@test.com", mockAdmin.Object.Email);
+            Assert.Equal("test123", mockAdmin.Object.Password);
+            Assert.Equal("Editor", mockAdmin.Object.Profile);
+        }
+    }
+}
---
+++ src/TestProject/Domain/Entities/VehicleTest.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using project.Domain.Entities;
+
+namespace TestProject.Domain.Entities
+{
+    public class VehicleTest
+    {
+        [Fact]
+        public void TestGetSetVehicleProperties()
+        {
+            // Arrange
+            var mockVehicle = new Mock<Vehicle>();
+
+            // Act
+            mockVehicle.Object.Id = 1;
+            mockVehicle.Object.Name = "Civic";
+            mockVehicle.Object.Make = "Honda";
+            mockVehicle.Object.ModelYear = 2014;
+            mockVehicle.Object.Color = "Red";
+            //mockVehicle.Object.Description = "Description";
+
+            // Assert
+            Assert.Equal(1, mockVehicle.Object.Id);
+            Assert.Equal("Civic", mockVehicle.Object.Name);
+            Assert.Equal("Honda", mockVehicle.Object.Make);
+            Assert.Equal(2014, mockVehicle.Object.ModelYear);
+            Assert.Equal("Red", mockVehicle.Object.Color);
+            //Assert.Equal("Description", mockVehicle.Object.Description);
+        }
+    }
+}
